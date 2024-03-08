065a6523c951fd9bf3cf784de6005777d77bfac6
Edem <edemq123@gmail.com>
2024-03-08 12:36:51 +0200

    Optional Chaining ( ?. )

--- data_structures_operators/script.js
+++ data_structures_operators/script.js
@@ -7,35 +7,35 @@ const flights =
 // Data needed for first part of the section
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
-// const openingHours = {
-//   thu: {
-//     open: 12,
-//     close: 22,
-//   },
-//   fri: {
-//     open: 11,
-//     close: 23,
-//   },
-//   sat: {
-//     open: 0, // Open 24 hours
-//     close: 24,
-//   },
-// };
-
 const openingHours = {
-  [weekdays[3]]: {
+  thu: {
     open: 12,
     close: 22,
   },
-  [weekdays[4]]: {
+  fri: {
     open: 11,
     close: 23,
   },
-  [`day-${2 + 4}`]: {
+  sat: {
     open: 0, // Open 24 hours
     close: 24,
   },
 };
+
+// const openingHours = {
+//   [weekdays[3]]: {
+//     open: 12,
+//     close: 22,
+//   },
+//   [weekdays[4]]: {
+//     open: 11,
+//     close: 23,
+//   },
+//   [`day-${2 + 4}`]: {
+//     open: 0, // Open 24 hours
+//     close: 24,
+//   },
+// };
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -73,8 +73,37 @@ const restaurant = {
   },
 };
 
+if (restaurant.openingHours && restaurant.openingHours.mon)
+  console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon.open);
+
+// WITH OPTIONAL CHAINING
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  // console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRissotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+// const users = [];
+const users = [{ name: 'Edem', email: 'edemq123@gmail.com' }];
+console.log(users[0]?.name ?? 'User array empty'); // RECOMMENDED
+
+if (users.length > 0) console.log(users[0].name); // Too much text
+else console.log('User array empty');
 /*
-// Looping aRRAYS: THE FOR-OF LOOP
+// Looping ARRAYS: THE FOR-OF LOOP
 
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
@@ -131,7 +160,7 @@ console.log(rest2);
 const guests = restaurant.numGuests || 10;
 console.log(guests);
 
-// Nullish: null and undefined (NOOT 0 OR ')
+// Nullish: null and undefined (NOT 0 OR '')
 const guestCorrect = restaurant.numGuests ?? 10;
 console.log(guestCorrect);
 
