1e526e97a210a2824499fd5326e5f5f275a7cba4
Edem <edemq123@gmail.com>
2024-03-08 14:32:53 +0200

    Coding Challenge #2

--- data_structures_operators/script.js
+++ data_structures_operators/script.js
@@ -73,6 +73,7 @@ const restaurant = {
   },
 };
 
+/*
 // Property NAMES
 const properties = Object.keys(openingHours);
 console.log(properties);
@@ -94,6 +95,8 @@ console.log(entries);
 for (const [day, { open, close }] of entries) {
   console.log(`On ${day} we open at ${open} and close at ${close}`);
 }
+*/
+
 /*
 //////////////////////////////////////////////
 // Optional Chaining
@@ -442,7 +445,50 @@ const game = {
   },
 };
 
+///////////////////////////////////////
+// Coding Challenge #2
+
 /* 
+Let's continue with our football betting app!
+
+1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
+      Odd of victory Bayern Munich: 1.33
+      Odd of draw: 3.25
+      Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
+
+BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
+      {
+        Gnarby: 1,
+        Hummels: 1,
+        Lewandowski: 2
+      }
+
+GOOD LUCK 😀
+*/
+
+// 1.
+for (const [goalNum, scorer] of game.scored.entries()) {
+  console.log(`Goal ${goalNum + 1}: ${scorer}`);
+}
+
+const odds = Object.values(game.odds);
+let average = 0;
+
+for (const odd of odds) average += odd;
+average /= odds.length;
+console.log(average);
+
+// 3.
+for (const [team, odd] of Object.entries(game.odds)) {
+  const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+  console.log(`Odd of ${teamStr} ${odd} `);
+}
+/* 
+///////////////////////////////////////
+// Coding Challenge #1
 We're building a football betting app (soccer for my American friends 😅)!
 
 Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
