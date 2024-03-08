4c54b4c2a791991df30bb8ac8fd315592896fa70
Edem <edemq123@gmail.com>
2024-03-08 15:14:12 +0200

    Maps: Fundermentals

--- data_structures_operators/script.js
+++ data_structures_operators/script.js
@@ -73,6 +73,39 @@ const restaurant = {
   },
 };
 
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+console.log(rest);
+rest.delete(2);
+// rest.clear();
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+// rest.set([1, 2], 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
+/*
 ///////////////////////
 // SETS
 const ordersSet = new Set([
@@ -105,6 +138,7 @@ console.log(
 );
 
 console.log(new Set('EdemQuashigah').size);
+*/
 
 /*
 // Property NAMES
