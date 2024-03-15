d001a5c62410d2ed3c0854552bc93e031431e6ac
Edem <edemq123@gmail.com>
2024-03-15 13:34:23 +0200

    Default Parameters

---
+++ functions/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5 OLD SCHOOL VIBES
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+// Skipping a parameter can be done with the undefined keyword.
+createBooking('LH123', undefined, 1000);
